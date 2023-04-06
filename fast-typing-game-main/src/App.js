aae8a1703efde6a6f403ac9d2648230715da5472
Adrian Imioło <105633316+adrian-imiolo@users.noreply.github.com>
2023-04-06 18:41:27 +0200

    Add files via upload

---
+++ fast-typing-game-main/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useRef } from "react"
+import useWordGame from "./hooks/useWordGame"
+import './styles.css'
+import { FaKeyboard } from "react-icons/fa"
+
+function App() {
+    const {
+        textBoxRef,
+        handleChange,
+        text,
+        isTimeRunning,
+        timeRemaining,
+        startGame,
+        wordCount,
+        displayedWords,
+        wordsMixer,
+        generateRandomWord,
+        writtenWordsArr,
+        enterRef
+    } = useWordGame(10)
+
+
+    return (
+        <div >
+            <h1 className="title">Fast Typing Game</h1>
+            <FaKeyboard size={60} />
+            {wordCount > 2 && !isTimeRunning ? <h1>Wow! You can rewrite: {wordCount * 6} words/min</h1> : <h1>How fast do you type?</h1>}
+            <div className="container">
+                <textarea
+
+                    value={isTimeRunning ? displayedWords : "Here words will be displayed"}
+                    disabled={!isTimeRunning}
+                />
+
+                <textarea
+                    ref={textBoxRef}
+                    onChange={handleChange}
+                    value={isTimeRunning ? text : "Press ENTER and rewrite text from box above as fast as you can!"}
+                    disabled={!isTimeRunning}
+                />
+            </div>
+            <h4>Time remaining: {timeRemaining}</h4>
+            <h1>Correct words count: {wordCount}</h1>
+            <button
+                onClick={startGame}
+                disabled={isTimeRunning}
+                ref={enterRef}
+            >
+                {wordCount > 2 && !isTimeRunning ? "Try again" : "Start"}
+            </button>
+
+
+        </div>
+    )
+}
+
+export default App
---
+++ fast-typing-game-main/src/hooks/useWordGame.js
@@ -0,0 +1,95 @@
+import { useState, useEffect, useRef } from "react"
+import wordlist from "../wordlist"
+
+function useWordGame(startingTime = 10) {
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(startingTime)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
+    const [displayedWords, setDisplayedWords] = useState("")
+    const textBoxRef = useRef(null)
+    const enterRef = useRef(null)
+
+    function handleChange(e) {
+        const { value } = e.target
+        setText(value)
+    }
+    const writtenWordsArr = text.split(" ")
+    let newArr = ""
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        const displayArr = displayedWords.trim().split(" ")
+        // const newArr = wordsArr.filter(word => word === displayedWords)
+        newArr = wordsArr.filter(word => {
+            for (let i = 0; i < displayArr.length; i++) {
+                if (word === displayArr[i]) {
+                    return word
+                }
+            }
+        })
+        return newArr.length
+    }
+
+    useEffect(() => {
+        enterRef.current.focus()
+    }, [])
+
+    const interval = setInterval(() => {
+        enterRef.current.focus()
+
+    }, 3000)
+
+    function startGame() {
+        setIsTimeRunning(true)
+        setTimeRemaining(startingTime)
+        setText("")
+        textBoxRef.current.disabled = false
+        textBoxRef.current.focus()
+        wordsMixer(words)
+        setWordCount(0)
+    }
+
+    function endGame() {
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
+        enterRef.current.disabled = true
+        setTimeout(() => { enterRef.current.disabled = false }, 2000)
+    }
+
+    useEffect(() => {
+        if (isTimeRunning && timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        } else if (timeRemaining === 0) {
+            endGame()
+        }
+
+    }, [timeRemaining, isTimeRunning])
+
+    useEffect(() => {
+        setWordCount(calculateWordCount(text))
+    }, [text])
+
+    const words = wordlist.split(' ')
+
+    function generateRandomWord(arr) {
+        let randomNumber = Math.floor(Math.random() * arr.length)
+        let randomWord = words[randomNumber]
+        return randomWord
+    }
+
+    function wordsMixer(arr) {
+        setDisplayedWords(prevWords => {
+            prevWords = new Array(40).fill(0).map(() => generateRandomWord(words))
+            // prevWords = [...prevWords]
+            // prevWords.push(rand(words), rand(words),rand(words),rand(words),rand(words))
+            let prevWordsSpace = prevWords.join(" ")
+            return prevWordsSpace
+        })
+
+    }
+    return { textBoxRef, handleChange, text, isTimeRunning, timeRemaining, startGame, wordCount, displayedWords, wordsMixer, generateRandomWord, writtenWordsArr, enterRef }
+}
+
+export default useWordGame
